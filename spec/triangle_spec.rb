a0b3733ca39a8d658376e6dd3c5eca5f524f480b
Morikiyo Inoha <morikiyo.inoha@gmail.com>
2018-09-10 17:20:19 +0900

    programmed trianble.rb and rspec-test.

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -1,7 +1,72 @@
 require 'spec_helper'
 
-RSpec.describe Triangle do
-  it "message the shape of trianble" do
-    expect(Triangle.determineShape([1,2,3])).to eq "三角形じゃないです＞＜"
+TARGET_FILE = 'triangle.rb'
+require_relative File.join('..', TARGET_FILE)
+
+describe Triangle do
+  describe "#determineShape" do
+    it "message Argument Error" do
+      message = "辺の長さは正の整数で指定してください"
+      expect(Triangle.determineShape(['5','3','x'])).to eq message
+      expect(Triangle.determineShape(['5','3','0'])).to eq message
+      expect(Triangle.determineShape(['5','3','-1'])).to eq message
+    end
+
+    it "message Not Triangle" do
+      message = "三角形じゃないです＞＜"
+      expect(Triangle.determineShape(['1','2'])).to eq message
+      expect(Triangle.determineShape(['1','2','4','1'])).to eq message
+    end
+
+    it "message Equilateral Triangle" do
+      message = "正三角形ですね！"
+      expect(Triangle.determineShape(['5','5','5'])).to eq message
+      expect(Triangle.determineShape(['173','173','173'])).to eq message
+    end
+
+    it "message Isosceles Triangle" do
+      message = "二等辺三角形ですね！"
+      expect(Triangle.determineShape(['3','3','5'])).to eq message
+      expect(Triangle.determineShape(['5','3','3'])).to eq message
+      expect(Triangle.determineShape(['3','5','3'])).to eq message
+    end
+
+    it "message Scalene Triangle" do
+      message = "不等辺三角形ですね！"
+      expect(Triangle.determineShape(['3','5','2'])).to eq message
+    end
+  end
+
+  describe "Command Line" do
+
+    let(:command) { File.join(File.dirname(__FILE__), "..", TARGET_FILE) }
+
+    it "print to stdout" do
+      expect {
+        puts `ruby #{command} 4, 3, 4`
+      }.to output("二等辺三角形ですね！\n").to_stdout
+
+      expect {
+        puts `ruby #{command} 4 3 4`
+      }.to output("二等辺三角形ですね！\n").to_stdout
+    end
+
+    it "print to stdout as in the problem example" do
+      expect {
+        puts `ruby #{command} 2, 3, 4`
+      }.to output("不等辺三角形ですね！\n").to_stdout
+
+      expect {
+        puts `ruby #{command} 2, 2, 1`
+      }.to output("二等辺三角形ですね！\n").to_stdout
+
+      expect {
+        puts `ruby #{command} 1, 1, 1`
+      }.to output("正三角形ですね！\n").to_stdout
+
+      expect {
+        puts `ruby #{command} 1, 2, 3, 4`
+      }.to output("三角形じゃないです＞＜\n").to_stdout
+    end
   end
 end
--- triangle.rb
+++ triangle.rb
@@ -1,11 +1,43 @@
 class Triangle
-  # lengths: 辺の長さの配列
-  def self.determineShape(lengths)
+  # args: 辺の長さの配列
+  def self.determineShape(args)
     # argument validation
-    # ...
-    # "不等辺三角形ですね"
-    # "二等辺三角形ですね"
-    # "正三角形ですね"
-    "三角形じゃないです＞＜"
+    lens = []
+    args.each {|arg|
+      len = Integer(arg) rescue nil
+      if len.nil? || len <= 0
+        return "辺の長さは正の整数で指定してください"
+      end
+      lens.push(len)
+    }
+
+    if lens.length != 3
+      return "三角形じゃないです＞＜"
+    end
+
+    if lens[0] == lens[1] && lens[1] == lens[2] && lens[2] == lens[0]
+      return "正三角形ですね！"
+    end
+
+    if lens[0] == lens[1] || lens[1] == lens[2] || lens[2] == lens[0]
+      return "二等辺三角形ですね！"
+    end
+
+    "不等辺三角形ですね！"
   end
+end
+
+### Command Line
+if $0 == __FILE__
+  # コマンドライン引数から末尾のカンマを除去する
+  args = []
+  ARGV.each {|arg|
+    if arg =~ /,$/
+      args.push(arg.chop)
+    else
+      args.push(arg)
+    end
+  }
+
+  puts Triangle.determineShape(args)
 end
